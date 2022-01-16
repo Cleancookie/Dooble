083e50e2157372a6c0248fcf658999b869d8ed8f
Cleancookie <pokemon999101@hotmail.co.uk>
2022-01-16 14:13:39 +0000

    💄 Start styling cards

--- src/components/Cards/DoobleCard.vue
+++ src/components/Cards/DoobleCard.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <span v-for="symbol in card" :key="symbol">{{ symbol }}</span>
+  <div class="card">
+    <span v-for="symbol in card" :key="symbol" class="symbol">{{
+      symbol
+    }}</span>
   </div>
 </template>
 
@@ -11,3 +13,24 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+.card {
+  display: flex;
+  outline: 1px solid black;
+  flex-wrap: wrap;
+  max-width: 22em;
+  min-height: 22em;
+}
+
+.symbol {
+  flex: 1 0 calc(33% - 2rem);
+  font-size: 3em;
+  padding: 1rem;
+
+  &:hover {
+    outline: 1px solid black;
+    cursor: pointer;
+  }
+}
+</style>
